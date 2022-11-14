3aeecfec17b36ad1cbe72e863d48982cc1855d25
Jack Pearce <jack@jackf.red>
2022-11-14 19:30:23 +0000

    Initial setup

---
+++ src/main/kotlin/io/github/chihirios/plushshark/PlushSharkMod.kt
@@ -0,0 +1,14 @@
+package io.github.chihirios.plushshark
+
+import net.fabricmc.api.ModInitializer
+import net.fabricmc.loader.api.FabricLoader
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+
+object PlushSharkMod : ModInitializer {
+    val LOGGER: Logger = LogManager.getLogger("Plush Shark")
+
+    override fun onInitialize() {
+        LOGGER.info("Plush shark mod version ${FabricLoader.getInstance().getModContainer("plushshark").get().metadata.version.friendlyString} loading")
+    }
+}
