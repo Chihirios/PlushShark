c147c376c8f444ced44a8559bebc9125a4f9f025
Jack Pearce <jack@jackf.red>
2022-11-18 02:42:29 +0000

    Rotation + Waterlog Blockstates

--- src/main/kotlin/io/github/chihirios/plushshark/blocks/BlahajBlock.kt
+++ src/main/kotlin/io/github/chihirios/plushshark/blocks/BlahajBlock.kt
@@ -1,16 +1,64 @@
 package io.github.chihirios.plushshark.blocks
 
 import net.minecraft.core.BlockPos
+import net.minecraft.core.Direction
+import net.minecraft.util.Mth
+import net.minecraft.world.item.context.BlockPlaceContext
 import net.minecraft.world.level.BlockGetter
+import net.minecraft.world.level.LevelAccessor
 import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.SimpleWaterloggedBlock
 import net.minecraft.world.level.block.state.BlockState
+import net.minecraft.world.level.block.state.StateDefinition
+import net.minecraft.world.level.block.state.properties.BlockStateProperties
+import net.minecraft.world.level.block.state.properties.BooleanProperty
+import net.minecraft.world.level.block.state.properties.IntegerProperty
+import net.minecraft.world.level.material.FluidState
+import net.minecraft.world.level.material.Fluids
 import net.minecraft.world.phys.shapes.CollisionContext
 import net.minecraft.world.phys.shapes.Shapes
 import net.minecraft.world.phys.shapes.VoxelShape
 
-open class BlahajBlock(properties: Properties) : Block(properties) {
+open class BlahajBlock(properties: Properties) : Block(properties), SimpleWaterloggedBlock {
+    val WATERLOGGED: BooleanProperty = BlockStateProperties.WATERLOGGED
+    val ROTATION: IntegerProperty = BlockStateProperties.ROTATION_16
 
+    init {
+        registerDefaultState(this.defaultBlockState().setValue(WATERLOGGED, false).setValue(ROTATION, 0))
+    }
+
+    @Suppress("OVERRIDE_DEPRECATION")
     override fun getShape(state: BlockState, level: BlockGetter, pos: BlockPos, context: CollisionContext): VoxelShape {
         return Shapes.create(0.25, 0.0, 0.1, 0.75, 3.0/16, 0.9)
+    }
+
+    override fun createBlockStateDefinition(builder: StateDefinition.Builder<Block, BlockState>) {
+        builder.add(WATERLOGGED, ROTATION)
+    }
+
+    @Suppress("OVERRIDE_DEPRECATION")
+    override fun getFluidState(state: BlockState): FluidState {
+        return if (state.getValue(WATERLOGGED)) Fluids.WATER.getSource(false) else super.getFluidState(state)
+    }
+
+    override fun updateShape(
+        state: BlockState,
+        direction: Direction,
+        neighborState: BlockState,
+        level: LevelAccessor,
+        currentPos: BlockPos,
+        neighborPos: BlockPos
+    ): BlockState {
+        if (state.getValue(WATERLOGGED)) {
+            level.scheduleTick(currentPos, Fluids.WATER, Fluids.WATER.getTickDelay(level))
+        }
+        return super.updateShape(state, direction, neighborState, level, currentPos, neighborPos)
+    }
+
+    override fun getStateForPlacement(context: BlockPlaceContext): BlockState? {
+        val fluidState = context.level.getFluidState(context.clickedPos)
+        // from StandingSignBlock#getStateForPlacement
+        val rotation = Mth.floor(((180.0f + context.rotation) * 16.0f / 360.0f).toDouble() + 0.5) and 15
+        return defaultBlockState().setValue(ROTATION, rotation).setValue(WATERLOGGED, fluidState.type === Fluids.WATER)
     }
 }
